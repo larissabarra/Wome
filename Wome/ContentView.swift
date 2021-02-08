3d29d52b2b8e472f7fddc65a6667189e022c771c
Larissa Barra <lari_conde94@hotmail.com>
2021-02-08 22:21:44 +0000

    Initial commit

---
+++ Wome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wome
+//
+//  Created by Larissa Barra Conde on 08/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
