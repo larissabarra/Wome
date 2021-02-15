bf64649b017a128b0fd037ac6c0f4651c412be9e
Larissa Barra <lari_conde94@hotmail.com>
2021-02-15 22:17:52 +0000

    Adjusting temperature entry flow

--- Wome/Views/ContentView.swift
+++ Wome/Views/ContentView.swift
@@ -8,28 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tabSelection = 0
-    
-    var body: some View {
-        TabView(selection: $tabSelection) {
-            TodayView()
-                .tabItem {
-                    Label("Today", systemImage: "thermometer")
-                }
-                .tag(0)
-         
-            Text("Chart Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Label("Chart", systemImage: "calendar")
-                }
-                .tag(1)
-        }
+  @Environment(\.managedObjectContext) var managedObjectContext
+  
+  @FetchRequest<Temperature>(
+    entity: Temperature.entity(),
+    sortDescriptors: [
+      NSSortDescriptor(keyPath: \Temperature.measurementTime, ascending: true)
+    ]
+  ) var temperatures
+  
+  @State private var tabSelection = 0
+  
+  var body: some View {
+    TabView(selection: $tabSelection) {
+      TodayView(onComplete: { temperature, time in
+        self.addTemperature(temperature, time)
+      })
+      .tabItem {
+        Label("Today", systemImage: "thermometer")
+      }
+      .tag(0)
+      
+      ChartView(temps: temperatures)
+      .tabItem {
+        Label("Chart", systemImage: "calendar")
+      }
+      .tag(1)
     }
+  }
+  
+  func addTemperature(_ temperature: Measurement<UnitTemperature>, _ time: Date) {
+    let newTemp = Temperature(context: managedObjectContext)
+    
+    newTemp.valueCelcius = temperature.converted(to: UnitTemperature.celsius).value
+    newTemp.valueFahrenheit = temperature.converted(to: UnitTemperature.fahrenheit).value
+    newTemp.measurementTime = time
+    
+    saveContext()
+  }
+  
+  func saveContext() {
+    do {
+      try managedObjectContext.save()
+    } catch {
+      print("Error saving managed object context: \(error)")
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
+
+
--- Wome/Views/TodayView.swift
+++ Wome/Views/TodayView.swift
@@ -8,13 +8,89 @@
 import SwiftUI
 
 struct TodayView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  static let dateFormat: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .full
+    return formatter
+  }()
+  
+  let onComplete: (Measurement<UnitTemperature>, Date) -> Void
+  let today = Date()
+  
+  @State var temperatureUnit: UnitTemperature = UnitTemperature.celsius
+  @State var temperatureTime: Date = Date()
+  @State var temperature: Double = 36.0
+  // TODO find a way use Measurement
+//  @State var temperature: Measurement<UnitTemperature> = Measurement(value: 36.0, unit: UnitTemperature.celsius)
+  
+  var temperatureProxy: Binding<String> {
+    Binding<String>(
+      get: {
+        String(format: "%.2f", self.temperature)
+      },
+      set: {
+        if let value = NumberFormatter().number(from: $0) {
+          self.temperature = value.doubleValue
+        }
+      }
+    )
+  }
+  
+  var body: some View {
+    Form {
+      Section{
+        Text("Today is \(today, formatter: Self.dateFormat)" )
+      }
+      Section(header: Text("Temperature")) {
+        HStack {
+          TextField("Temperature", text: temperatureProxy)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .keyboardType(.decimalPad)
+          Picker("Unit", selection: $temperatureUnit) {
+            Text(UnitTemperature.celsius.symbol).tag(UnitTemperature.celsius)
+            Text(UnitTemperature.fahrenheit.symbol).tag(UnitTemperature.fahrenheit)
+          }
+          .pickerStyle(SegmentedPickerStyle())
+          .onChange(of: temperatureUnit) { unit in
+            var u: UnitTemperature
+            if unit.symbol == UnitTemperature.celsius.symbol {
+              u = UnitTemperature.fahrenheit
+            } else {
+              u = UnitTemperature.celsius
+            }
+            self.temperature = Measurement(value: self.temperature, unit: u).converted(to: unit).value
+          }
+        }
+        
+        DatePicker(
+          "Measurement time:",
+          selection: $temperatureTime,
+          displayedComponents: .hourAndMinute)
+        
+        HStack {
+          Spacer()
+          Button("Save") {
+            hideKeyboard()
+            addTemperature()
+          }
+        }
+      }
+      Text("\(temperature)")
     }
+  }
+  
+  private func addTemperature() {
+    onComplete(
+      Measurement(value: self.temperature, unit: self.temperatureUnit),
+      temperatureTime
+    )
+  }
 }
 
-struct TodayView_Previews: PreviewProvider {
-    static var previews: some View {
-        TodayView()
-    }
+#if canImport(UIKit)
+extension View {
+  func hideKeyboard() {
+    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+  }
 }
+#endif
